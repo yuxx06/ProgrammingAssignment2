cd59623c3637a1f5b1c9418dfa37e48933400e63
yuxx06 <yuxx06@gmail.com>
2015-02-21 08:12:13 -0800

    Update makeCacheMatrix.R

--- makeCacheMatrix.R
+++ makeCacheMatrix.R
@@ -1,12 +1,18 @@
 makeCacheMatrix <- function(x = numeric()) {
+        # cached value is initially set to NULL
         m <- NULL
+        # store a matrix
         setMatrix <- function(y) {
                 x <<- y
                 m <<- NULL
         }
+        # return the stored matrix
         getMatrix <- function() x
+        # cache the argument
         setInverse <- function(solve) m <<- solve
+        # get the inverse matrix 
         getInverse <- function() m
+        # return list which can call the corresponding matrix
         list(setInverse = setInverse, getInverse = getInverse,
              setMatrix = setMatrix,
              getMatrix = getMatrix)
